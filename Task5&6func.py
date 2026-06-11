ecbf44b9b39c8dd6e1665381d730f976026d977f
Siddiq Shaik <144650997+shaik-siddiq13@users.noreply.github.com>
2026-06-11 17:33:20 +0530

    Added new files

--- Task5&6func.py
+++ Task5&6func.py
@@ -376,48 +376,386 @@ print(word_frequency(data))
 
 # 45. Pass a list of dictionaries and return the dictionary with the maximum value for a given key.
 
-def max_by_key
+def max_by_key(data,key_name):
+    return max(data, key=lambda item: item[key_name])
 
-
-
-
-
-
-
+students = [{"name": "Siddiq", "marks": 80},{"name": "Bob", "marks": 95},{"name": "Mahesh", "marks": 75}]
+print(max_by_key(students,"marks"))
 
 # 46. Write a function that takes a string and returns a dictionary with counts of each word.
 
+def word_count(text):
+    words = text.split()
+    freq = {}
 
+    for word in words:
+        freq[word] = freq.get(word,0) + 1
+    return freq
 
-
-
-
-
-
+print(word_count("hello world hello python"))
 
 # 47. Create a function that accepts a list of numbers and returns a tuple of (sum, average, max, min).
 
+def statistics(numbers):
+    total = sum(numbers)
+    avg = total / len(numbers)
 
-
-
-
-
-
-
+    return (total, avg, max(numbers), min(numbers))
+nums = [10,20,30,40]
+print(statistics(nums))
 
 # 48. Write a function that takes a tuple of strings and returns a list of strings sorted by length.
 
-
+def sort_by_length(words):
+    return sorted(words, key = len)
+names = ("apple","banana","cherry","date")
+print(sort_by_length(names))
 
 # 49. Pass a dictionary of tuples and return the tuple with the maximum length.
 
+def longest_tuple(data):
+    return max(data.values(),key = len)
+items = {"a": (1, 2),"b": (1, 2, 3, 4),"c": (5,)}
 
-
-
-
+print(longest_tuple(items))
 
 # 50. Write a function that takes a list of strings and returns a dictionary grouping them by their first letter.
 
+def group_by_first_letter(words):
+    result = {}
+
+    for word in words:
+        first = word[0]
+
+        if first not in result:
+            result[first] = []
+
+        result[first].append(word)
+    return result
+names = ["apple","banana","cherry","date","avocado"]
+print(group_by_first_letter(names))
+
+
+#  Integer Function Questions (51–60)
+# 51. Write a function that takes an integer and returns whether it is prime.
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    
+    for i in range(2,number):
+        if number % i == 0:
+            return False
+    return True
+print(is_prime(7))
+
+# 52. Pass an integer to a function and return its factorial.
+
+def factorial(number):
+    result = 1
+
+    for i in range(1,number + 1):
+        result *= i
+    return result 
+print(factorial(5))
+
+# 53. Create a function that accepts an integer and returns the sum of its digits.
+
+def sum_of_digits(number):
+    total = 0
+
+    for digit in str(number):
+        total += int(digit)
+    return total
+print(sum_of_digits(1234))
+
+# 54. Write a function that takes an integer and returns whether it is an Armstrong number.
+
+def is_armstrong(number):
+    digits = str(number)
+    power = len(digits)
+
+    total = sum(int(digit) ** power for digit in digits)
+
+    return total == number
+print(is_armstrong(153))
+
+# 55. Pass an integer and return the reverse of its digits.
+
+def reverse_digits(number):
+    return int(str(number)[::-1])
+print(reverse_digits(12345)) 
+
+# 56. Write a function that takes an integer and returns whether it is a palindrome number.
+
+def is_palindrome_number(number):
+    return str(number) == str(number)[::-1]
+print(is_palindrome_number(12321))
+
+# 57. Create a function that accepts an integer and returns the count of even digits in it.
+
+def count_even_digits(number):
+    count = 0
+
+    for digit in str(number):
+        if int(digit) % 2 == 0:
+            count += 1
+    return count
+
+print(count_even_digits(248913))
+        
+# 58. Write a function that takes an integer and returns the next Fibonacci number after it.
+
+def next_fibonacci(number):
+    a, b = 0, 1
+
+    while b <= number:
+        a, b = b, a + b
+    return b
+print(next_fibonacci(10))
+
+# 59. Pass an integer and return the greatest common divisor (GCD) of that integer and another fixed number.
+
+def gcd(number):
+    fixed_number = 12
+
+    while fixed_number:
+        number, fixed_number = fixed_number, number % fixed_number
+    return number
+print(gcd(36))
+
+# 60. Write a function that takes an integer and returns a list of all its divisors.
+
+def divisiors(number):
+    result = []
+
+    for i in range(1, number + 1):
+        if number % i == 0:
+            result.append(i)
+    return result
+print(divisiors(12))
+
+
+#  Map (10 Coding Questions)
+# 1. Use map() to square each number in [2, 4, 6, 8].
+
+numbers = [2, 4, 6, 8, 10]
+result = list(map(lambda x: x ** 2, numbers))
+print(result)
+
+# 2. Convert an array of strings ["apple", "banana", "cherry"] into uppercase using map().
+
+fruits = ["apple", "banana", "cherry"]
+result = list(map(lambda x: x.upper(),fruits))
+print(result)
+
+# 3. Extract the id property from an array of objects: [{id:1,name:"A"}, {id:2,name:"B"}, {id:3,name:"C"}]
+
+data = [{"id":1,"name":"A"}, {"id":2,"name":"B"}, {"id":3,"name":"C"}]
+result = list(map(lambda x: x["id"], data))
+print(result)
+
+# 4. Add 5 to each element in [10, 20, 30] using map().
+
+numbers = [10,20,30]
+result = list(map(lambda x: x + 5, numbers))
+print(result)
+
+# 5. Convert an array of numbers [1,2,3,4] into strings using map().
+
+numbers = [1,2,3,4]
+result = list(map(str,numbers))
+print(result)
+
+# 6. Use map() to append "!" to each word in ["hi","hello","hey"].
+
+words = ["hi","hello","hey"]
+result = list(map(lambda x: x + "!", words))
+print(result)
+
+# 7. Create a new array of lengths from ["dog","elephant","cat"].
+
+animals = ["dog","elephant","cat"]
+result = list(map(len, animals))
+print(result)
+
+# 8. Use map() to transform [true,false,true] into ["YES","NO","YES"].
+
+values = [True, False, True]
+result = list(map(lambda x: "YES" if x else "NO", values))
+print(result)
+
+# 9. Given [1,2,3], use map() to return [1,4,9].
+
+numbers = [1,2,3]
+result = list(map(lambda x: x ** 2, numbers))
+print(result)
+
+# 10. Use map() to add a fullName property to each object in:[{first:"John",last:"Doe"}, {first:"Jane",last:"Smith"}]
+
+people = [{"first":"John","last":"Doe"}, {"first":"Jane","last":"Smith"}]
+result = list(map(lambda x : {**x,"fullName": x["first"] + " " + x["last"]}, people))
+print(result)
+
+
+#  Filter (10 Coding Questions)
+# 1. Use filter() to get even numbers from [1,2,3,4,5,6].
+
+numbers = [1,2,3,4,5,6]
+result = list(filter(lambda x: x % 2 == 0, numbers))
+print(result)
+
+# 2. Filter out words shorter than 4 letters from ["hi","hello","hey","world"].
+
+words = ["hi","hello","hey","world"]
+result = list(filter(lambda x : len(x)<= 4, words))
+print(result)
+
+# 3. From [10,25,30,45], filter numbers greater than 20.
+
+numbers = [10,25,30,45]
+result = list(filter(lambda x: x > 20, numbers))
+print(result)
+
+# 4. Use filter() to get only truthy values from [0,1,false,2,"",3].
+
+values = [0,1,False,2,"",3]
+result = list(filter(bool, values))
+print(result)
+
+# 5. Filter out negative numbers from [5,-3,9,-1,0].
+
+numbers = [5,-3,9,-1,0]
+result = list(filter(lambda x: x <= 0, numbers))
+print(result)
+
+# 6. Use filter() to get names starting with "A" from ["Alice","Bob","Andrew","Charlie"].
+
+names = ["Alice","Bob","Andrew","Charlie"]
+result = list(filter(lambda x: x.startswith("A"), names))
+print(result)
+
+# 7. From [100,200,300,400], filter numbers divisible by 200.
+
+numbers = [100,200,300,400]
+result = list(filter(lambda x: x % 200 == 0, numbers))
+print(result)
+
+# 8. Use filter() to get objects with age > 18 from:[{name:"Tom",age:15},{name:"Jerry",age:20}]
+
+people = [{"name":"Tom","age":15},{"name":"Jerry","age":20}]
+result = list(filter(lambda x: x["age"] > 18, people))
+print(result)
+
+# 9. Filter out duplicate values from [1,2,2,3,4,4,5] using filter().
+
+numbers = [1,2,2,3,4,4,5]
+result = list(filter(lambda x: numbers.count(x) == 1, numbers)) 
+print(result)
+
+# ===================================
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+seen = []
+
+result = list(filter(lambda x: x not in seen and not seen.append(x), numbers))
+
+print(result)
+# ======================================================
+
+# 10. From ["red","blue","green","yellow"], filter colors containing "e".
+
+colors = ["red","blue","green","yellow"]
+result = list(filter(lambda x : "e" in x, colors))
+print(result)
+
+#  Reduce (10 Coding Questions)
+# 1. Use reduce() to sum [1,2,3,4,5].
+
+from functools import reduce
+numbers = [1,2,3,4,5]
+result = reduce(lambda x, y: x + y, numbers)
+print(result)
+
+# 2. Find the maximum number in [10,25,30,5] using reduce().
+
+from functools import reduce
+numbers = [10,25,30,5]
+result = reduce(lambda x,y: x if x > y else y, numbers)
+print(result)
+
+# 3. Use reduce() to concatenate ["a","b","c"] into "abc".
+
+from functools import reduce
+letters = ["a","b","c"]
+result = reduce(lambda x,y: x + y, letters)
+print(result)
+
+# 4. Count occurrences of each element in [1,2,2,3,3,3] using reduce().
+
+from functools import reduce
+
+numbers = [1, 2, 2, 3, 3, 3]
+
+result = reduce(lambda acc, x: {**acc,x: acc.get(x, 0) + 1},numbers,{})
+
+print(result)
+
+
+# ============================================
+from functools import reduce
+numbers = [1, 2, 2, 3, 3, 3]
+def count_occurrences(acc, x):
+    acc[x] = acc.get(x, 0) + 1
+    return acc
+result = reduce(count_occurrences, numbers, {})
+print(result)
+# =====================================================
+
+# 5. Use reduce() to flatten [[1,2],[3,4],[5]] into [1,2,3,4,5].
+
+from functools import reduce
+lists = [[1,2],[3,4],[5]]
+result = reduce(lambda x,y: x + y, lists)
+print(result)
+
+# 6. Calculate the product of [2,3,4] using reduce().
+
+from functools import reduce
+numbers = [2,3,4]
+result = reduce(lambda x,y : x * y, numbers)
+print(result)
+
+# 7. Use reduce() to find the longest word in ["cat","elephant","dog"].
+
+from functools import reduce
+words = ["cat","elephant","dog"]
+result = reduce(lambda x,y: x if len(x) > len(y) else y, words)
+print(result)
+
+# 8. Build an object mapping names to ages from:[{name:"Tom",age:15},{name:"Jerry",age:20}]
+
+from functools import reduce
+people = [{"name":"Tom","age":15},{"name":"Jerry","age":20}]
+
+result = reduce(lambda acc, x: {**acc,x["name"]: x["age"]},people,{})
+print(result)
+# ===========================================
+
+# 9. Use reduce() to reverse ["a","b","c"] into "cba".
+
+from functools import reduce
+letters = ["a","b","c"]
+result = reduce(lambda x, y: y + x, letters)
+print(result)
+
+# 10. Calculate the average of [10,20,30,40] using reduce().
+
+from functools import reduce
+numbers = [10,20,30,40]  
+total = reduce(lambda x,y : x + y, numbers)
+average = total / len(numbers)
+print(average)
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,57 @@
-from functools import reduce
-numbers = [10,20,30,40]
-total = reduce(lambda x,y : x + y, numbers)
-average = total / len(numbers)
-print(average)
 
+class Library:
+
+    # Class Variable
+    library_name = "Central Library"
+
+    # Constructor
+    def __init__(self, book_name, author, price):
+        self.book_name = book_name
+        self.author = author
+        self.price = price
+
+    # Instance Method
+    def display_book(self):
+        print("Book Name :", self.book_name)
+        print("Author    :", self.author)
+        print("Price     :", self.price)
+
+    # Instance Method
+    def update_price(self, new_price):
+        self.price = new_price
+        print("Price Updated Successfully")
+
+    # Class Method
+    @classmethod
+    def get_library_name(cls):
+        return cls.library_name
+
+    # Static Method
+    @staticmethod
+    def discount(price, percent):
+        return price - (price * percent / 100)
+
+
+# Create Objects
+b1 = Library("Python Programming", "Guido van Rossum", 1000)
+b2 = Library("SQL Basics", "John Smith", 800)
+
+# Display Book Details
+b1.display_book()
+print("-" * 30)
+
+b2.display_book()
+print("-" * 30)
+
+# Update Price
+b1.update_price(1200)
+
+print("\nUpdated Book Details")
+b1.display_book()
+
+# Class Method
+print("\nLibrary Name:", Library.get_library_name())
+
+# Static Method
+discount_price = Library.discount(1200, 10)
+print("Price After 10% Discount:", discount_price)
